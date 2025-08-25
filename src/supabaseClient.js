0065034faac1937c126cc39f283701d895c8b2e1
Dennon Parks <parksdennon@gmail.com>
2025-08-24 23:02:11 -0600

    added create client

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -6,4 +6,7 @@ const SUPABASE_ANON_KEY = process.env.REACT_APP_SUPABASE_ANON_KEY;
 console.log("Supabase URL:", process.env.REACT_APP_SUPABASE_URL);
 console.log("Supabase Anon Key:", process.env.REACT_APP_SUPABASE_ANON_KEY);
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(
+  process.env.REACT_APP_SUPABASE_URL,
+  process.env.REACT_APP_SUPABASE_ANON_KEY
+)
