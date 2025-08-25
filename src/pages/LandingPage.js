8f15cead2e0afa3623cbe509ef798cff81e2dc72
Dennon Parks <parksdennon@gmail.com>
2025-08-25 00:04:03 -0600

    fixed landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -19,6 +19,7 @@ export default function LandingPage({ user }) {
           .from("games")
           .select("*")
           .order("game_date", { ascending: true });
+          console.log("Fetched games:", games);
 
         if (error) throw error;
 
