3d3a313089ebd0e04bf503bf253ff860f05fe4aa
Dennon Parks <parksdennon@gmail.com>
2025-08-24 23:34:13 -0600

    login fix

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,12 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
-const SUPABASE_ANON_KEY = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-console.log("Supabase URL:", process.env.REACT_APP_SUPABASE_URL);
-console.log("Supabase Anon Key:", process.env.REACT_APP_SUPABASE_ANON_KEY);
+console.log("Supabase URL:", SUPABASE_URL);
+console.log("Supabase Anon Key:", SUPABASE_ANON_KEY);
 
-export const supabase = createClient(
-  process.env.REACT_APP_SUPABASE_URL,
-  process.env.REACT_APP_SUPABASE_ANON_KEY
-)
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
